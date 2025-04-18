2873c6bd3ba8e08aa95cd47d8b64ac3dd8976a53
Parv Maurya <parv.maurya@gmail.com>
2025-04-18 19:52:13 +0530

    resolved alpha beta tree traversal bugs

--- src/main/java/org/foresight/ChessArticle.java
+++ src/main/java/org/foresight/ChessArticle.java
@@ -2,141 +2,93 @@ package org.foresight;
 
 import com.github.bhlangonijr.chesslib.*;
 import com.github.bhlangonijr.chesslib.move.Move;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.*;
-import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class ChessArticle {
 
+    private static final Logger logger = LoggerFactory.getLogger(ChessArticle.class);
+
     static int count = 0;
 
-    static int[] PIECE_VALUE = {10, 30, 32, 50, 90, 0, -10, -30, -32, -50, -90, 0, 0};
-    static long CENTER_MASK = (1<<18) | (1<<19) | (1<<20) | (1<<21) | (1<<26) | (1<<27) | (1<<28) | (1<<29) | (1<<34) | (1<<35) | (1<<36) | (1<<37) | (1<<42) | (1<<43) | (1<<44) | (1<<45);
-    static long CENTER_CENTER_MASK = (1<<27) | (1<<28) | (1<<35) | (1<<36);
-
-    static int generateWhiteBishopMoves(Board b) {
-        long whiteBishops = b.getBitboard(Piece.WHITE_BISHOP);
-        long blackBishops = b.getBitboard(Piece.BLACK_BISHOP);
-
-        ArrayList<Integer> whiteBishopsPosition = new ArrayList<>();
-        ArrayList<Integer> blackBishopsPosition = new ArrayList<>();
-        for(int bishopIndex = 0; bishopIndex<64; bishopIndex = bishopIndex + 1){
-            if ((whiteBishops & (1L << bishopIndex)) !=0) whiteBishopsPosition.add(bishopIndex);
-            if ((blackBishops & (1L << bishopIndex)) !=0) blackBishopsPosition.add(bishopIndex);
-        }
-
-        long occupiedPieces = b.getBitboard();
-        int[] bishopPositionOffset = {9, 7, -9, -7};
-        int count_moves = 0;
-
-        for (Integer position: whiteBishopsPosition) {
-            for (int offset : bishopPositionOffset) {
-                for (int multiplier = 1; (position + offset * multiplier)<64 & (position + offset * multiplier)>0; multiplier++) {
-                    int pos = position + (offset * multiplier);
-
-                    if ((occupiedPieces & (1L << pos)) != 0) {
-                        count_moves++;
-                    }
-                    }
-                }
-            }
-        for (Integer position: blackBishopsPosition) {
-            for (int offset : bishopPositionOffset) {
-                for (int multiplier = 1; (position + offset * multiplier)<64 & (position + offset * multiplier)>0; multiplier++) {
-                    int pos = position + (offset * multiplier);
-
-                    if ((occupiedPieces & (1L << pos)) != 0) {
-                        count_moves--;
-                    }
-                }
-            }
-        }
-        return count_moves;
-        }
 
     static int getBoardValue(Board board) {
-        long whiteBishops = board.getBitboard(Piece.WHITE_BISHOP);
-        long whiteKnights = board.getBitboard(Piece.WHITE_KNIGHT);
-        long whiteQueen = board.getBitboard(Piece.WHITE_QUEEN);
-        long whiteRooks = board.getBitboard(Piece.WHITE_ROOK);
-        long whitePawns = board.getBitboard(Piece.WHITE_PAWN);
-
-        long blackBishops = board.getBitboard(Piece.BLACK_BISHOP);
-        long blackKnights = board.getBitboard(Piece.BLACK_KNIGHT);
-        long blackQueen = board.getBitboard(Piece.BLACK_QUEEN);
-        long blackRooks = board.getBitboard(Piece.BLACK_ROOK);
-        long blackPawns = board.getBitboard(Piece.BLACK_PAWN);
+        ChessBitboard chessBitboard = new ChessBitboard(board);
 
         int score = 0;
-        score = score + Long.bitCount(whiteBishops) * 320;
-        score = score + Long.bitCount(whiteKnights) * 300;
-        score = score + Long.bitCount(whiteQueen) * 900;
-        score = score + Long.bitCount(whiteRooks) * 500;
-        score = score + Long.bitCount(whitePawns) * 100;
-        score = score - Long.bitCount(blackBishops) * 320;
-        score = score - Long.bitCount(blackKnights) * 300;
-        score = score - Long.bitCount(blackQueen) * 900;
-        score = score - Long.bitCount(blackRooks) * 500;
-        score = score - Long.bitCount(blackPawns) * 100;
-
-        score = score + (Long.bitCount(CENTER_MASK & board.getBitboard(Piece.WHITE_KNIGHT)) * 50)
-                    - (Long.bitCount(CENTER_MASK & board.getBitboard(Piece.BLACK_KNIGHT)) * 50);
-
-        score = score + (Long.bitCount(CENTER_CENTER_MASK & board.getBitboard(Piece.WHITE_PAWN)) * 10)
-                - (Long.bitCount(CENTER_MASK & board.getBitboard(Piece.BLACK_PAWN)) * 10);
-
-        score = score + (Long.bitCount(CENTER_CENTER_MASK & board.getBitboard(Piece.WHITE_KNIGHT)) * 20)
-                - (Long.bitCount(CENTER_MASK & board.getBitboard(Piece.BLACK_KNIGHT)) * 20);
-
-        score = score + generateWhiteBishopMoves(board) * 10;
+        score = score + chessBitboard.getStaticScore();
+        score = score + chessBitboard.getCenterScore();
+        score = score + chessBitboard.generateNetBishopMoves() * 10;
+        score = score + chessBitboard.generateNetRookMoves() * 20;
+        score = score + chessBitboard.getDoubledPawnsPenalty() * 20;
+        score = score + chessBitboard.getIsolatedPawnsPenalty() * 20;
+        score = score + chessBitboard.getSpaceScore() * 2;
         return score;
     }
 
     static FastHashing hashing = new FastHashing();
     static int intermediateNodes = 0;
     static int MAX_TIME = 20000;
-    static long startTime = 0;
 
     public static Move findBestMoveRoot(Board board) {
-        startTime = System.currentTimeMillis();
+        logger.info(" ");
+        logger.info(" New Move ");
+        long startTime = System.currentTimeMillis();
 
         List<Move> bestMovesList = board.legalMoves();
         List<Integer> countList = new ArrayList<>();
+        boolean isTimeout = false;
 
         boolean isMaximiser = Side.WHITE.equals(board.getSideToMove());
         Move bestMove = bestMovesList.get(0);
         for (int depth = 1; depth <= 20 && System.currentTimeMillis() - startTime < MAX_TIME; depth = depth + 1) {
+            Move tmpBestMove = bestMove;
             hashing.clearHits();
             long start = System.nanoTime();
-            int alpha = -10000;
-            int beta = 10000;
+            int alpha = -100000;
+            int beta = 100000;
 
             count = 0;
             qnodes = 0;
             Map<Move, Integer> moveMap = new HashMap<>();
             long depthStartTime = System.currentTimeMillis();
-
             for (Move move: bestMovesList) {
                 intermediateNodes = 0;
                 count = count + 1;
-
                 board.doMove(move);
-                int currentScore = findMoveAndScore(board, depth, alpha, beta, !isMaximiser);
+                int currentScore = 0;
+                if (hashing.getScore(board.getZobristKey(), depth) == null) {
+                    currentScore = findMoveAndScore(board, depth, alpha, beta, !isMaximiser, startTime);
+                } else {
+                    currentScore = hashing.getScore(board.getZobristKey(), depth);
+                }
                 board.undoMove();
-
-                if (System.currentTimeMillis() - startTime > MAX_TIME) return bestMove;
+                if (System.currentTimeMillis() - startTime > MAX_TIME) { System.out.println("Hogaya Timeout"); isTimeout = true; break;}
 
                 if (isMaximiser && alpha < currentScore) {
-                    bestMove = move;
+                    tmpBestMove = move;
                     alpha = currentScore;
                 } else if (!isMaximiser && beta > currentScore){
-                    bestMove = move;
+                    tmpBestMove = move;
                     beta = currentScore;
                 }
                 moveMap.put(move, currentScore);
             }
-            hashing.addScore(board.getZobristKey(), (isMaximiser ? alpha : beta), depth-1, bestMove);
+            if (isTimeout) break;
+            bestMovesList = moveMap
+                            .entrySet()
+                            .stream()
+                            .sorted(Map.Entry.comparingByValue())
+                            .map(Map.Entry::getKey)
+                            .collect(Collectors.toList());
+            if (isMaximiser) {
+                Collections.reverse(bestMovesList);
+            }
+            hashing.addScore(board.getZobristKey(), (isMaximiser ? alpha : beta), depth-1, tmpBestMove);
 
             // Set TT
             long zkey = board.getZobristKey();
@@ -147,40 +99,52 @@ public class ChessArticle {
             }
 
             // Set PV move, i.e best move
-            bestMovesList.remove(bestMove);
-            bestMovesList.add(0, bestMove);
+            bestMovesList.remove(tmpBestMove);
+            bestMovesList.add(0, tmpBestMove);
 
             countList.add(count);
 
             // Return if checkmate found
-            if (Side.WHITE.equals(board.getSideToMove()) && alpha == 10000) { return bestMove;}
-            else if (Side.BLACK.equals(board.getSideToMove()) && beta == -10000) { return bestMove; }
+            if (Side.WHITE.equals(board.getSideToMove()) && alpha >= 10000) {
+                System.out.println("White checkmate found"); break;}
+            else if (Side.BLACK.equals(board.getSideToMove()) && beta <= -10000) {
+                System.out.println("Black checkmate found"); break; }
+
+            if (System.currentTimeMillis() - startTime < MAX_TIME) {
+                bestMove = tmpBestMove;
+            }
 
             // log relevant metrics
-            System.out.print("Depth: " + depth);
-            System.out.print(" " + bestMove);
-            System.out.print(" " + (System.currentTimeMillis() - depthStartTime)/1000.0);
-            System.out.print(" Node count : " + count);
-            System.out.print(" Score: " + moveMap.get(bestMovesList.get(0)));
-            System.out.printf(" Hashing Hit ratio: %,.2f ", hashing.getHitsRatio() * 100);
-            System.out.print(" Positions added: " + hashing.getTotalAdds());
-            System.out.print(" NPS: " + (1_000_000_000L * count / ((System.nanoTime() - start) * 1000)) + "k" );
-            System.out.println(" Qnodes% :" + qnodes*100/(count+qnodes) + "%");
+            logger.info("Depth: {} | Node count: {} | Score: {} | Time: {}s | BestMove: {} | Hashing Hit ratio: {}% | Positions added: {} | NPS: {}k | Qnodes%: {}%",
+                    depth+1,
+                    count,
+                    moveMap.get(bestMovesList.get(0)),
+                    (System.currentTimeMillis() - depthStartTime) / 1000.0,
+                    bestMove,
+                    hashing.getHitsRatio() * 100,
+                    hashing.getTotalAdds(),
+                    1_000_000_000L * count / ((System.nanoTime() - start) * 1000),
+                    (count + qnodes) == 0 ? 0 : qnodes * 100 / (count + qnodes)
+            );
         }
 
-        System.out.println("Branching factor: " + (1.0*countList.stream().mapToInt(i -> i).sum()/(countList.stream().mapToInt(i -> i).sum()-countList.get(countList.size()-1))));
+        logger.info("Branching factor: {}", 1.0 * countList.stream().mapToInt(i -> i).sum() / (countList.stream().mapToInt(i -> i).sum() - countList.get(countList.size() - 1)));
+        logger.info("Best move returned: {}", bestMove);
         return bestMove;
     }
 
-    public static int findMoveAndScore(Board board, int depth, int alpha, int beta, boolean isMaximizer) {
+    public static int findMoveAndScore(Board board, int depth, int alpha, int beta, boolean isMaximizer, long startTime) {
         count = count + 1;
         intermediateNodes++;
         if (System.currentTimeMillis() - startTime > MAX_TIME) {
-            return isMaximizer ? alpha : beta;
+            return quiescence(board, alpha, beta, depth, isMaximizer, 3);
         }
 
-        if(board.isMated()) { return Side.WHITE.equals(board.getSideToMove()) ? -10000 : 10000;}
+        if(board.isMated()) { return Side.WHITE.equals(board.getSideToMove()) ? -10000+depth : 10000-depth;}
         if(board.isDraw()) { return 0; }
+        if (board.isRepetition(3)) {return 0;}
+
+        int bestScore = !isMaximizer ? Integer.MAX_VALUE : Integer.MIN_VALUE;
 
         long zkey = board.getZobristKey();
         Integer cachedScore = hashing.getScore(zkey, depth);
@@ -191,15 +155,17 @@ public class ChessArticle {
         if(depth == 0) {
 //            return getBoardValue(board);
             int scr = quiescence(board, alpha, beta, depth, isMaximizer, 3);
+            hashing.addScore(board.getZobristKey(), scr, 0, null);
             return scr;
-//            hashing.addScore(board.getZobristKey(), scr, 0, null);
         }
 
         int reductionFactor = 2;
         if (!isMaximizer) {
-            if (depth - reductionFactor - 1 >= 0 & !board.isKingAttacked()) {
+            if (depth > 3 && !board.isKingAttacked()) {
                 board.doNullMove();
-                int nullmovescore = findMoveAndScore(board, depth - reductionFactor - 1, beta-1, beta, true);
+                int nullmovescore = findMoveAndScore(board, depth - 2, beta-1, beta, true, startTime);
+                // alpha is the best move of maximiser yet, beta is best move for minimiser
+                // black skips a move, in that case, white made 2 moves in a row
                 board.undoMove();
                 if (nullmovescore >= beta) {
                     return nullmovescore;
@@ -207,9 +173,11 @@ public class ChessArticle {
             }
         }
         else {
-            if (depth - reductionFactor - 1 >= 0 & !board.isKingAttacked()) {
+            if (depth >= 3 && !board.isKingAttacked()) {
+                // minimiser skips a move, means black skips his move, alpha is the best move of white till now, beta is best black move
+                // black skips a move, white makes a move, white ka score should be really well
                 board.doNullMove();
-                int nullmovescore = findMoveAndScore(board, depth - reductionFactor - 1, alpha, alpha+1, false);
+                int nullmovescore = findMoveAndScore(board, depth - 2, alpha, alpha+1, false, startTime);
                 board.undoMove();
                 if (alpha >= nullmovescore) {
                     return nullmovescore;
@@ -218,6 +186,8 @@ public class ChessArticle {
         }
 
         List<Move> legalMovesList = board.legalMoves();
+        int alphaOriginal = alpha;
+        int betaOriginal = beta;
 
         if (isMaximizer) {
             Move bestMove = null;
@@ -229,8 +199,11 @@ public class ChessArticle {
 
             for(Move move : legalMovesList) {
                 board.doMove(move);
-                int score = findMoveAndScore(board, depth-1, alpha, beta, false);
+                int score = findMoveAndScore(board, depth-1, alpha, beta, false, startTime);
                 board.undoMove();
+                if (score > bestScore) {
+                    bestScore = score;
+                }
 
                 if(score > alpha) {
                     bestMove = move;
@@ -239,8 +212,8 @@ public class ChessArticle {
 
                 if (alpha >= beta) { break;}
             }
-            hashing.addScore(board.getZobristKey(), alpha, depth-1, bestMove);
-            return alpha;
+            if (bestScore > alphaOriginal && bestScore < betaOriginal) hashing.addScore(board.getZobristKey(), bestScore, depth, bestMove);
+            return bestScore;
         } else {
             Move bestMove = null;
 
@@ -252,8 +225,11 @@ public class ChessArticle {
 
             for(Move move : legalMovesList) {
                 board.doMove(move);
-                int score = findMoveAndScore(board, depth-1, alpha, beta, true);
+                int score = findMoveAndScore(board, depth-1, alpha, beta, true, startTime);
                 board.undoMove();
+                if (score < bestScore) {
+                    bestScore = score;
+                }
 
                 if(score < beta) {
                     bestMove = move;
@@ -262,8 +238,9 @@ public class ChessArticle {
 
                 if (alpha >= beta) { break; }
             }
-            hashing.addScore(board.getZobristKey(), beta, depth-1, bestMove);
-            return beta;
+
+            if (bestScore > alphaOriginal && bestScore < betaOriginal) hashing.addScore(board.getZobristKey(), bestScore, depth, bestMove);
+            return bestScore;
         }
 
 
@@ -288,7 +265,7 @@ public class ChessArticle {
         }
 
         for (Move move: board.legalMoves()) {
-            if (!board.getPiece(move.getTo()).equals(Piece.NONE)) {
+            if (!board.getPiece(move.getTo()).equals(Piece.NONE) | board.isKingAttacked() ) {
 
                 board.doMove(move);
                 evaluationScore = quiescence(board, alpha, beta, depth, !isMaximizer, quiescenceDepth-1);
